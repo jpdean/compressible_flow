6db6b54328cb13b740c2f4734a7ef88680e4d11e
Joe Dean <jpd62@cam.ac.uk>
2026-04-01 14:56:27 +0100

    Add time stepping

--- euler.py
+++ euler.py
@@ -3,8 +3,8 @@ from ufl import dx, dS, FacetNormal, dot
 from mpi4py import MPI
 from dolfinx import mesh, fem, io
 import numpy as np
-from dolfinx.fem.petsc import NonlinearProblem
-from meshing import create_naca_4digit, create_naca_4digit_3d
+from dolfinx.fem.petsc import NonlinearProblem, LinearProblem
+from meshing import create_naca_4digit, create_naca_4digit_3d, create_biconvex, create_blade_row, create_circle
 
 
 def conserved_vars(U):
@@ -58,16 +58,20 @@ def phi(n_F, U_m, U_p, alpha):
 
 gamma = 1.4
 comm = MPI.COMM_WORLD
+# "forward_euler", "backward_euler", or "steady_state"
+time_stepping = "steady_state"
 
-# msh, ft, boundaries = create_naca_4digit(
-#     comm, h_near=0.01, h_far=0.2, r_near=0.8, r_far=2.5)
+# If time_stepping is "steady_state", then num_timesteps is ignored
+num_timesteps = 200
 
-msh, ft, boundaries = create_naca_4digit_3d(comm)
+msh, ft, boundaries = create_naca_4digit(
+    comm, h_near=0.01, h_far=0.2, r_near=0.8, r_far=2.5, show_gui=False)
 
 dim = msh.topology.dim
 ds = ufl.Measure("ds", domain=msh, subdomain_data=ft)
 
-V = fem.functionspace(msh, ("Discontinuous Lagrange", 0, (dim + 2,)))
+k = 0
+V = fem.functionspace(msh, ("Discontinuous Lagrange", k, (dim + 2,)))
 
 U = fem.Function(V)
 v = ufl.TestFunction(V)
@@ -92,7 +96,7 @@ R = 287.0
 T_0 = 300.0
 p_0 = 101325.0
 rho_0 = p_0 / (R * T_0)
-M = fem.Constant(msh, 0.3)
+M = fem.Constant(msh, 0.1)
 if M.value < 1.0:
     # Subsonic inlet
     c_inf = (gamma * R * T_0)**0.5
@@ -138,8 +142,6 @@ alpha_wall = abs(dot(u, n)) + c
 F += ufl.inner(phi(n, U, U_star_wall, alpha_wall), v) * \
     ds((boundaries["walls"], boundaries["airfoil"]))
 
-residual = F  # - ufl.inner(f, v) * dx
-
 c0 = np.sqrt(gamma*R*T_0)
 u0 = M.value * c0
 rho0 = p_0/(R*T_0)
@@ -151,39 +153,101 @@ U.interpolate(lambda x: np.array([
     rho0 * E0 * np.ones_like(x[0])
 ]))
 
-petsc_options = {
-    "snes_type": "newtonls",
-    "ksp_type": "preonly",
-    "pc_type": "lu",
-    "pc_factor_mat_solver_type": "mumps",
-    "ksp_error_if_not_converged": True,
-    "snes_monitor": None
-}
-problem = NonlinearProblem(
-    residual, U, petsc_options_prefix="euler_", petsc_options=petsc_options
-)
+V_vis = fem.functionspace(msh, ("Discontinuous Lagrange", k + 1))
+V_vis_vec = fem.functionspace(msh, ("Discontinuous Lagrange", k + 1, (dim,)))
 
-U = problem.solve()
-converged_reason = problem.solver.getConvergedReason()
-assert converged_reason > 0
-
-V_vis = fem.functionspace(msh, ("Discontinuous Lagrange", 1))
-V_vis_vec = fem.functionspace(msh, ("Discontinuous Lagrange", 1, (dim,)))
+# Temperature
+T = p / (R * rho)
 
 # Write to file
-for name, var, space in (("rho", rho, V_vis), ("p", p, V_vis), ("u", u, V_vis_vec)):
+vis_data = []
+for name, var, space in (("rho", rho, V_vis), ("p", p, V_vis), ("u", u, V_vis_vec), ("T", T, V_vis)):
     expr = fem.Expression(var, space.element.interpolation_points)
     f = fem.Function(space)
-    f.interpolate(expr)
-    with io.VTXWriter(msh.comm, f"{name}.bp", f) as file:
+    file = io.VTXWriter(msh.comm, f"{name}.bp", f)
+    vis_data.append((expr, f, file))
+
+if time_stepping == "forward_euler":
+    U_np1 = ufl.TrialFunction(V)
+    delta_t = fem.Constant(msh, 1e-6)
+    F += ufl.inner((U_np1 - U) / delta_t, v) * dx
+
+    a = ufl.lhs(F)
+    L = ufl.rhs(F)
+
+    U_h = fem.Function(V)
+    problem = LinearProblem(a, L, u=U_h, petsc_options_prefix="euler_", petsc_options={
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "mumps",
+        "ksp_error_if_not_converged": True})
+
+    t = 0.0
+    for i in range(num_timesteps):
+        t += delta_t.value
+        print(f"step = {i}, t={t}")
+        problem.solve()
+        U.x.array[:] = U_h.x.array[:]
+
+        if i % 100 == 0:
+            for (expr, f, file) in vis_data:
+                f.interpolate(expr)
+                file.write(t)
+elif time_stepping == "steady_state":
+    petsc_options = {
+        "snes_type": "newtonls",
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "mumps",
+        "ksp_error_if_not_converged": True,
+        "snes_monitor": None
+    }
+    problem = NonlinearProblem(
+        F, U, petsc_options_prefix="euler_", petsc_options=petsc_options
+    )
+
+    U = problem.solve()
+    converged_reason = problem.solver.getConvergedReason()
+    assert converged_reason > 0
+
+    for (expr, f, file) in vis_data:
+        f.interpolate(expr)
         file.write(0.0)
 
-with io.XDMFFile(comm, "aerofoil_mesh.xdmf", "w") as file:
-    file.write_mesh(msh)
-    file.write_meshtags(ft, msh.geometry)
+    # with io.XDMFFile(comm, "aerofoil_mesh.xdmf", "w") as file:
+    #     file.write_mesh(msh)
+    #     file.write_meshtags(ft, msh.geometry)
+elif time_stepping == "backward_euler":
+    delta_t = fem.Constant(msh, 1e-3)
+    U_n = fem.Function(V)
+    U_n.interpolate(U)
 
-# error = U - g_d_expr(x)
-# e_L2 = (msh.comm.allreduce(fem.assemble_scalar(
-#     fem.form(ufl.inner(error, error) * ufl.dx)), op=MPI.SUM))**0.5
+    F += ufl.inner((U - U_n) / delta_t, v) * dx
 
-# print(f"L2 error: {e_L2}")
+    petsc_options = {
+        "snes_type": "newtonls",
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "mumps",
+        "ksp_error_if_not_converged": True,
+        "snes_monitor": None
+    }
+    problem = NonlinearProblem(
+        F, U, petsc_options_prefix="euler_", petsc_options=petsc_options
+    )
+
+    t = 0.0
+    for i in range(num_timesteps):
+        t += delta_t.value
+        print(f"step = {i}, t={t}")
+        problem.solve()
+        converged_reason = problem.solver.getConvergedReason()
+        assert converged_reason > 0
+        U_n.x.array[:] = U.x.array[:]
+        if i % 2 == 0:
+            for (expr, f, file) in vis_data:
+                f.interpolate(expr)
+                file.write(t)
+
+for (_, _, file) in vis_data:
+    file.close()
